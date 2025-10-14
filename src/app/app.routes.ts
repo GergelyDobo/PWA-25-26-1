15b127befeb5ea949844e7eb0912eef11417bb96
csaba.kiss <csaba.kiss@casrd.net>
2025-10-14 09:19:29 +0200

    Új route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,25 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { maxBuildingsGuard } from './shared/max-buildings.guard';
 
 export const routes: Routes = [
-    {
-        path: "main",
-        component: MainPageComponent
-    },
-    {
-        path: "shop",
-        loadComponent: () => import("./building-shop/building-shop.component").then(c => c.BuildingShopComponent),
-        canActivate: [maxBuildingsGuard]
-    },
-    {
-        path: "**",
-        redirectTo: "main"
-    }
+  {
+    path: 'main',
+    component: MainPageComponent,
+  },
+  {
+    path: 'shop',
+    loadComponent: () =>
+      import('./building-shop/building-shop.component').then(
+        (c) => c.BuildingShopComponent
+      ),
+    canActivate: [maxBuildingsGuard],
+  },
+  {
+    path: 'settings',
+    loadComponent: () =>
+      import('./settings/settings.component').then((c) => c.SettingsComponent),
+  },
+  {
+    path: '**',
+    redirectTo: 'main',
+  },
 ];
