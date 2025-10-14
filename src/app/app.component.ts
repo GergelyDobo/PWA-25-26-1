80026d78399b3c3b69d553139ec26e876d8f7114
csaba.kiss <csaba.kiss@casrd.net>
2025-10-14 09:21:15 +0200

    Header és menü

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
-import { MainPageComponent } from './main-page/main-page.component';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIconButton } from '@angular/material/button';
+import {
+  MatDrawer,
+  MatDrawerContainer,
+  MatDrawerContent,
+} from '@angular/material/sidenav';
+import { MatIcon } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet]
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    MatToolbar,
+    MatIcon,
+    MatDrawerContainer,
+    MatDrawer,
+    MatListModule,
+    MatDrawerContent,
+    MatIconButton,
+  ],
 })
-export class AppComponent {}
+export class AppComponent {
+  private router = inject(Router);
+  createBuilding() {
+    this.router.navigateByUrl('shop');
+  }
+  openSettings(): void {
+    this.router.navigateByUrl('settings');
+  }
+}
