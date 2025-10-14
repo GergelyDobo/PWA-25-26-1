79b454aa431c7db65ce8013137c898672755d3cc
csaba.kiss <csaba.kiss@casrd.net>
2025-10-14 09:22:16 +0200

    Build shop style

--- src/app/building-shop/building-shop.component.ts
+++ src/app/building-shop/building-shop.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -12,30 +17,34 @@ import { Router } from '@angular/router';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   templateUrl: './building-shop.component.html',
-  styleUrl: './building-shop.component.scss'
+  styleUrl: './building-shop.component.scss',
 })
 export class BuildingShopComponent {
   public buildingForm: FormGroup;
 
-  constructor(formBuilder: FormBuilder, private ms: ManagementService, private router: Router) {
+  constructor(
+    formBuilder: FormBuilder,
+    private ms: ManagementService,
+    private router: Router
+  ) {
     this.buildingForm = formBuilder.group({
-      name: ["", [Validators.required, Validators.minLength(3)]],
+      name: ['', [Validators.required, Validators.minLength(3)]],
       income: [1],
-      price: [10, Validators.min(10)]
+      price: [10, Validators.min(10)],
     });
   }
 
   public onSubmit() {
-    if(this.buildingForm.valid) {
+    if (this.buildingForm.valid) {
       this.ms.createBuilding(
         this.buildingForm.value.name,
         this.buildingForm.value.income,
         this.buildingForm.value.price
       );
-      this.router.navigateByUrl("main");
+      this.router.navigateByUrl('main');
     }
   }
 }
